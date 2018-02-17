c7c1c8086866c578c50d34e5b0097c6b68ced516
Justin Hong <jhong2009@gmail.com>
2018-02-16 17:39:02 -0800

    upload longest word exercise

---
+++ Basic Algorithms/longestWord.js
@@ -0,0 +1,42 @@
+// Return the length of the longest word in the provided sentence.
+// Your response should be a number.
+
+function findLongestWord(str) {
+	var len = 0;
+	// convert str into array
+	var words = str.split(' ');
+
+	// compare lengths of each word
+	for (let i = 0; i < words.length; i++) {
+		if (words[i].length > len) {
+			len = words[i].length;
+		}
+	}
+	return len;
+}
+
+function findLongestWord(str) {
+	var words = str.split(' ');
+	var len = [];
+
+	// push word lengths to len
+	for (let i = 0; i < words.length; i++) {
+		len.push(words[i].length);
+	}
+
+	// sort from smallest to largest length
+	len.sort(function(a,b) {
+		return a - b;
+	});
+
+	// largest length is last value in len
+	return len[len.length - 1];
+}
+
+
+// test cases
+findLongestWord("The quick brown fox jumped over the lazy dog"); // 6
+findLongestWord("May the force be with you"); // 5
+findLongestWord("Google do a barrel roll"); // 6
+findLongestWord("What is the average airspeed velocity of an unladen swallow"); // 8
+findLongestWord("What if we try a super-long word such as otorhinolaryngology"); // 19
