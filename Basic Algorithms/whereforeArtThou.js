d0c632dca08eb922b27551009b3afad6bd077002
Justin Hong <jhong2009@gmail.com>
2018-03-01 18:08:53 -0800

    upload solution using filter and every

--- Basic Algorithms/whereforeArtThou.js
+++ Basic Algorithms/whereforeArtThou.js
@@ -40,6 +40,17 @@ function whatIsInAName(collection, source) {
 	});
 }
 
+function whatIsInAName(collection, source) {
+
+	let srcKeys = Object.keys(source);
+
+	return collection.filter(function(obj) {
+		return srcKeys.every(function(key) {
+			return key in obj && obj[key] === source[key]; 
+        })
+    });
+}
+
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
 // [{ first: "Tybalt", last: "Capulet" }]
 whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 });
