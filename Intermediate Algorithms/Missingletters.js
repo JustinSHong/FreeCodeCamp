5f4d13e4090430401e1086e211d26da1ac729828
Justin Hong <jhong2009@gmail.com>
2018-03-03 19:54:30 -0800

    upload solution using for loop, String.fromCharCode(), str.charCodeAt()

---
+++ Intermediate Algorithms/Missingletters.js
@@ -0,0 +1,24 @@
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
+function fearNotLetter(str) {
+	let missing;
+	let min = str.charCodeAt(0);
+	let max = str.charCodeAt(str.length - 1);
+	let index = 0;
+	
+	for (let i = min; i <= max; i++) {
+		if (str[index] !== String.fromCharCode(i)) {
+			missing = String.fromCharCode(i);
+			return missing;
+		}
+		index++;
+	}
+	return undefined;
+}
+
+fearNotLetter("abce"); // "d"
+fearNotLetter("abcdefghjklmno"); // "i"
+fearNotLetter("bcd"); // undefined
+fearNotLetter("yz"); // undefined
