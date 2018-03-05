3182b955621f5f43e25351998e9aa475b759815d
Justin Hong <jhong2009@gmail.com>
2018-03-04 17:22:06 -0800

    upload solution using for loop and switch()

---
+++ Intermediate Algorithms/HtmlEntities.js
@@ -0,0 +1,43 @@
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe)
+ // in a string to their corresponding HTML entities.
+
+ function convertHTML(str) {
+	
+	for (let letter of str) {
+		switch (letter) {
+            case '&':
+				str = str.replace(letter, "&amp;");
+				break;
+            case '<':
+				str = str.replace(letter, "&lt;");
+				break;
+            case '>':
+				str = str.replace(letter, "&gt;");
+				break;
+            case '"':
+				str = str.replace(letter, "&quot;");
+				break;
+            case "'":
+				str = str.replace(letter, "&apos;");
+				break;
+		}
+	}
+	return str;
+}
+
+convertHTML("Dolce & Gabbana");
+// Dolce &​amp; Gabbana
+convertHTML("Hamburgers < Pizza < Tacos");
+// Hamburgers &​lt; Pizza &​lt; Tacos
+convertHTML("Sixty > twelve");
+// Sixty &​gt; twelve
+convertHTML('Stuff in "quotation marks"');
+// Stuff in &​quot;quotation marks&​quot;
+convertHTML("Shindler's List");
+// Shindler&​apos;s List
+convertHTML("<>"); 
+// &​lt;&​gt;
+convertHTML("abc");
+// abc 
+
+
