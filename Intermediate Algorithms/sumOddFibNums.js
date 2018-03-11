e74283faaa1d0da6613e059855180d678abbc9d5
Justin Hong <jhong2009@gmail.com>
2018-03-10 16:26:57 -0800

    upload solution using loops

---
+++ Intermediate Algorithms/sumOddFibNums.js
@@ -0,0 +1,31 @@
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+// The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers.
+// The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+function sumFibs(num) {
+	let fibNums = [1, 1, 2, 3, 5, 8];
+	let sum = 0;
+	let index = 0;
+	
+	// add more fibonacci nums
+	let i = 5;
+	while (i <= num) {
+		i++;
+		fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+	}
+	
+	// add all odd fibonnaci nums
+	for (let n of fibNums) {
+		if (n <= num && n % 2 === 1) {
+			sum += n;
+		}
+	}
+	return sum;
+}
+
+sumFibs(1); // 1 
+sumFibs(1000); // 1785
+sumFibs(4000000); // 4613732
+sumFibs(4); // 5
+sumFibs(75024); // 60696
+sumFibs(75025); // 135721  
