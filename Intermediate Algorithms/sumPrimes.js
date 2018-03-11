2ef0d8a707475a2f5278054cfdd0d1195f8a806d
Justin Hong <jhong2009@gmail.com>
2018-03-10 19:33:22 -0800

    added comments

--- Intermediate Algorithms/sumPrimes.js
+++ Intermediate Algorithms/sumPrimes.js
@@ -1,8 +1,9 @@
-Sum all the prime numbers up to and including the provided number.
+// Sum all the prime numbers up to and including the provided number.
 
-A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+// A prime number is defined as a number greater than one and having only two divisors, one and itself.
+// For example, 2 is a prime number because it's only divisible by one and two.
 
-The provided number may not be a prime.
+// The provided number may not be a prime.
 
 function sumPrimes(num) {
 	let primes = [2, 3];
@@ -11,6 +12,7 @@ function sumPrimes(num) {
 		return num;
 	}
 	
+	// add all numbers to prime that are not divisible by 2 and 3
 	for (let i = 4; i <= num; i++) {
 		if (i % 2 !== 0) {
 			if (i % 3 !== 0) {
@@ -19,6 +21,8 @@ function sumPrimes(num) {
         }
 	}
 
+	// check primes for multiples of prime numbers
+	// splice out all multiples of primes
 	for (let i = primes.length - 1; i >= 0; i--) {
 		for (let j = 0; j < primes.length; j++) {
 			if (primes[i] % primes[j] === 0 &&
@@ -27,7 +31,8 @@ function sumPrimes(num) {
 			}
 		}
 	}
-	console.log(primes);
+	// sum all primes
+	// console.log(primes);
 	return primes.reduce(function(a,b) {
 		return a + b;
 	});
