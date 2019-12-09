9647107f3114689f6e03111ac4962e50129c186d
JustinSHong <jhong2009@gmail.com>
2019-12-08 16:10:21 -0800

    checkPalindromes: it should correctly determine if the input string is a palindrome

--- __tests__/checkPalindromes.test.ts
+++ __tests__/checkPalindromes.test.ts
@@ -20,6 +20,21 @@ describe('Determines if the given input string is a palindrome', () => {
 		'five|_/|four'
 	]
 
+	const solutions: { [key: string]: boolean } = {
+		eye: true,
+		_eye: true,
+		'race car': true,
+		'not a palindrome': false,
+		'A man, a plan, a canal. Panama': true,
+		'never odd or even': true,
+		nope: false,
+		almostomla: false,
+		'My age is 0, 0 si ega ym.': true,
+		'1 eye for of 1 eye.': false,
+		'0_0 (: /- :) 0-0': true,
+		'five|_/|four': false
+	}
+
 	describe.each([testCases])('takes a string as input', a => {
 		it('should take a string as input', () => {
 			expect(typeof a).toBe('string')
@@ -29,6 +44,12 @@ describe('Determines if the given input string is a palindrome', () => {
 			expect(typeof palindrome(a)).toBe('boolean')
 			expect(typeof palindrome2(a)).toBe('boolean')
 			expect(typeof palindrome3(a)).toBe('boolean')
+		})
+
+		it('should correctly determine if the input string is a palindrome', () => {
+			expect(palindrome(a)).toBe(solutions[a])
+			expect(palindrome2(a)).toBe(solutions[a])
+			expect(palindrome3(a)).toBe(solutions[a])
 		})
 	})
 })
