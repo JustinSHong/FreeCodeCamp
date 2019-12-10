5ab07c86ee15eefe807e8a6143d2d8c06f7c21c1
JustinSHong <jhong2009@gmail.com>
2019-12-09 21:08:21 -0800

    correctly use describe.each for testcases

--- __tests__/caesarCipher.test.ts
+++ __tests__/caesarCipher.test.ts
@@ -2,31 +2,27 @@ import { rot13 } from '../BasicAlgorithms/caesarCipher'
 
 describe('Decypts the ROT13 cipher and returns the decoded string', () => {
 	const testCases = [
-		'SERR PBQR PNZC',
-		'SERR CVMMN!',
-		'SERR YBIR?',
-		'GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.'
+		['SERR PBQR PNZC', 'FREE CODE CAMP'],
+		['SERR CVMMN!', 'FREE PIZZA!'],
+		['SERR YBIR?', 'FREE LOVE?'],
+		[
+			'GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.',
+			'THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX.'
+		]
 	]
 
-	const solutions: { [key: string]: string } = {
-		'SERR PBQR PNZC': 'FREE CODE CAMP',
-		'SERR CVMMN!': 'FREE PIZZA!',
-		'SERR YBIR?': 'FREE LOVE?',
-		'GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.':
-      'THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX.'
-	}
-
-	describe.each([testCases])('takes a string as input', a => {
+	describe.each(testCases)('takes a string as input', (a, b) => {
 		it('should take a string as input', () => {
 			expect(typeof a).toBe('string')
 		})
 
-		it('should return the decoded string as output', () => {
-			expect(typeof rot13(a)).toBe('string')
-		})
+		const result = rot13(a)
 
-		it('should return the correct decoded string as output', () => {
-			expect(rot13(a)).toBe(solutions[a])
+		it('should return a string as output', () => {
+			expect(typeof result).toBe('string')
+		})
+		it(`should take ${a} and return ${b}`, () => {
+			expect(result).toBe(b)
 		})
 	})
 })
