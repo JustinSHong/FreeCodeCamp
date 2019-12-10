4469f10099a8ebd03ec107b6d6ac2bcd13795f68
JustinSHong <jhong2009@gmail.com>
2019-12-09 20:27:09 -0800

    chunkyMonkey: it should take a number as an argument

--- __tests__/chunkyMonkey.test.ts
+++ __tests__/chunkyMonkey.test.ts
@@ -5,18 +5,22 @@
 
 describe('Splits an array in to groups the length of size and returns them as a 2D array', () => {
 	const testCases = [
-		['a', 'b', 'c', 'd'],
-		[0, 1, 2, 3, 4, 5],
-		[0, 1, 2, 3, 4, 5],
-		[0, 1, 2, 3, 4, 5],
-		[0, 1, 2, 3, 4, 5, 6],
-		[0, 1, 2, 3, 4, 5, 6, 7, 8],
-		[0, 1, 2, 3, 4, 5, 6, 7, 8]
+		[['a', 'b', 'c', 'd'], 2],
+		[[0, 1, 2, 3, 4, 5], 3],
+		[[0, 1, 2, 3, 4, 5], 2],
+		[[0, 1, 2, 3, 4, 5], 4],
+		[[0, 1, 2, 3, 4, 5, 6], 3],
+		[[0, 1, 2, 3, 4, 5, 6, 7, 8], 4],
+		[[0, 1, 2, 3, 4, 5, 6, 7, 8], 2]
 	]
 
 	describe.each([testCases])('takes an array and a number as input', a => {
 		it('should take an array as an argument', () => {
-			expect(Array.isArray(a)).toBe(true)
+			expect(Array.isArray(a[0])).toBe(true)
+		})
+
+		it('should take a number as an argument', () => {
+			expect(typeof a[1]).toBe('number')
 		})
 	})
 })
