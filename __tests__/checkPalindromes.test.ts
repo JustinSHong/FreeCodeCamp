b618f0a802750deb39fceaff5e20ffaba82a1c31
JustinSHong <jhong2009@gmail.com>
2019-12-09 21:02:09 -0800

    correctly use describe.each testcases

--- __tests__/checkPalindromes.test.ts
+++ __tests__/checkPalindromes.test.ts
@@ -6,50 +6,39 @@ import {
 
 describe('Determines if the given input string is a palindrome', () => {
 	const testCases = [
-		'eye',
-		'_eye',
-		'race car',
-		'not a palindrome',
-		'A man, a plan, a canal. Panama',
-		'never odd or even',
-		'nope',
-		'almostomla',
-		'My age is 0, 0 si ega ym.',
-		'1 eye for of 1 eye.',
-		'0_0 (: /- :) 0-0',
-		'five|_/|four'
+		['eye', true],
+		['_eye', true],
+		['race car', true],
+		['not a palindrome', false],
+		['A man, a plan, a canal. Panama', true],
+		['never odd or even', true],
+		['nope', false],
+		['almostomla', false],
+		['My age is 0, 0 si ega ym.', true],
+		['1 eye for of 1 eye.', false],
+		['0_0 (: /- :) 0-0', true],
+		['five|_/|four', false]
 	]
 
-	const solutions: { [key: string]: boolean } = {
-		eye: true,
-		_eye: true,
-		'race car': true,
-		'not a palindrome': false,
-		'A man, a plan, a canal. Panama': true,
-		'never odd or even': true,
-		nope: false,
-		almostomla: false,
-		'My age is 0, 0 si ega ym.': true,
-		'1 eye for of 1 eye.': false,
-		'0_0 (: /- :) 0-0': true,
-		'five|_/|four': false
-	}
-
-	describe.each([testCases])('takes a string as input', a => {
+	describe.each(testCases)('takes a string as input', (a, b) => {
 		it('should take a string as input', () => {
 			expect(typeof a).toBe('string')
 		})
 
+		const result = palindrome(a as string)
+		const result2 = palindrome2(a as string)
+		const result3 = palindrome3(a as string)
+
 		it('should return a boolean as output', () => {
-			expect(typeof palindrome(a)).toBe('boolean')
-			expect(typeof palindrome2(a)).toBe('boolean')
-			expect(typeof palindrome3(a)).toBe('boolean')
+			expect(typeof result).toBe(typeof b)
+			expect(typeof result2).toBe('boolean')
+			expect(typeof result3).toBe('boolean')
 		})
 
-		it('should correctly determine if the input string is a palindrome', () => {
-			expect(palindrome(a)).toBe(solutions[a])
-			expect(palindrome2(a)).toBe(solutions[a])
-			expect(palindrome3(a)).toBe(solutions[a])
+		it(`takes ${a} and returns ${b}`, () => {
+			expect(result).toBe(b)
+			expect(result2).toBe(b)
+			expect(result3).toBe(b)
 		})
 	})
 })
