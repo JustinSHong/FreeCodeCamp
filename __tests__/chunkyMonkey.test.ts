506349ca591eeb64aa2e55a0c101c8695363c592
JustinSHong <jhong2009@gmail.com>
2019-12-10 20:04:34 -0800

    add expected values to testcases

--- __tests__/chunkyMonkey.test.ts
+++ __tests__/chunkyMonkey.test.ts
@@ -1,17 +1,18 @@
 // import {
-//     chunkArrayInGroups,
-//     chunkArrayInGroups2
-// } from "../BasicAlgorithms/chunkyMonkey";
+// 	chunkArrayInGroups,
+// 	chunkArrayInGroups2
+// } from '../BasicAlgorithms/chunkyMonkey'
 
 describe('Splits an array in to groups the length of size and returns them as a 2D array', () => {
+	// prettier-ignore
 	const testCases = [
-		[['a', 'b', 'c', 'd'], 2],
-		[[0, 1, 2, 3, 4, 5], 3],
-		[[0, 1, 2, 3, 4, 5], 2],
-		[[0, 1, 2, 3, 4, 5], 4],
-		[[0, 1, 2, 3, 4, 5, 6], 3],
-		[[0, 1, 2, 3, 4, 5, 6, 7, 8], 4],
-		[[0, 1, 2, 3, 4, 5, 6, 7, 8], 2]
+		[['a', 'b', 'c', 'd'], 2, [['a', 'b'], ['c', 'd']]],
+		[[0, 1, 2, 3, 4, 5], 3, [[0, 1, 2], [3, 4, 5]]],
+		[[0, 1, 2, 3, 4, 5], 2, [[0, 1], [2, 3], [4, 5]]],
+		[[0, 1, 2, 3, 4, 5], 4, [[0, 1, 2, 3], [4, 5]]],
+		[[0, 1, 2, 3, 4, 5, 6], 3, [[0, 1, 2], [3, 4, 5], [6]]],
+		[[0, 1, 2, 3, 4, 5, 6, 7, 8], 4, [[0, 1, 2, 3], [4, 5, 6, 7], [8]]],
+		[[0, 1, 2, 3, 4, 5, 6, 7, 8], 2, [[0, 1], [2, 3], [4, 5], [6, 7], [8]]]
 	]
 
 	describe.each([testCases])('takes an array and a number as input', a => {
