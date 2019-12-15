c6907a738545f0e4097b62cc25d044006b532427
JustinSHong <jhong2009@gmail.com>
2019-12-15 14:08:31 -0800

    makeAPerson: add instructions and test cases

---
+++ IntermediateAlgorithms/makeAPerson.ts
@@ -0,0 +1,12 @@
+// Create a constructor function with the following methods
+// getFirstName()
+// getLastName()
+// getFullName()
+// setFirstName(first)
+// setLastName(last)
+// setFullName(firstAndLast)
+
+// const bob = new Person('Bob Ross');
+// bob.getFirstName(); // Bob
+// bob.getLastName(); // Ross
+// bob.getFullName(); // Bob Ross
