fc2986654be15073b5b7bcf2bf4793199fec80bc
JustinSHong <jhong2009@gmail.com>
2019-12-15 15:50:29 -0800

    makeAPerson: should have a method to get first name

--- __tests__/makeAPerson.test.ts
+++ __tests__/makeAPerson.test.ts
@@ -1,5 +1,5 @@
 import { Person } from '../IntermediateAlgorithms/makeAPerson'
-
+// prettier-ignore
 describe('it should create a Person instance with methods to set and to retrieve first and last name', () => {
 	const bob = new Person('Bob Ross')
 	const alice = new Person('Alice Kinneman')
@@ -14,14 +14,21 @@ describe('it should create a Person instance with methods to set and to retrieve
 	]
 
 	describe.each(testCases)('create a Person given their full name', (a, b) => {
-		it('should have a method to get a person\'s first name', () => {})
-		it('should have a method to get a person\'s last name', () => {})
-		it('should have a method to get a person\'s full name', () => {})
-		it('should have a method to set a person\'s first name', () => {})
-		it('should have a method to set a person\'s last name', () => {})
-		it('should have a method to set a person\'s full name', () => {})
 		it(`should take ${a} and return ${b}`, () => {
 			expect(a).toBe(b)
 		})
 	})
+
+	it('should have a method to get a person\'s first name', () => {
+		expect(bob.getFirstName).toBeTruthy()
+		expect(typeof bob.getFirstName).toBe('function')
+		expect(alice.getFirstName).toBeTruthy()
+		expect(typeof alice.getFirstName).toBe('function')
+	})
+
+	it('should have a method to get a person\'s last name', () => {})
+	it('should have a method to get a person\'s full name', () => {})
+	it('should have a method to set a person\'s first name', () => {})
+	it('should have a method to set a person\'s last name', () => {})
+	it('should have a method to set a person\'s full name', () => {})
 })
