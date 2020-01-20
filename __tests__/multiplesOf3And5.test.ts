326a5109cb83fb0e1cba3ee48a89ac17b74ea701
JustinSHong <jhong2009@gmail.com>
2020-01-19 19:01:40 -0800

    add unit test

--- __tests__/multiplesOf3And5.test.ts
+++ __tests__/multiplesOf3And5.test.ts
@@ -1,10 +1,19 @@
-import { multiplesOf3And5 } from '../ProjectEuler/multiplesOf3And5'
+import { multiplesOf3and5 } from '../ProjectEuler/multiplesOf3And5'
 
-describe('Returns the sum of all the multiples of 3 and 5', () => {
+describe('multiplesOf3And5', () => {
 	const testCases = [
 		[1000, 233168],
 		[49, 543],
 		[19564, 89301183],
-		[8456, 16687353],
+		[8456, 16687353]
 	]
+
+	describe.each(testCases)(
+		'Returns the sum of all the multiples of 3 and 5',
+		(a, b) => {
+			it(`should take a number ${a} and return ${b}`, () => {
+				expect(multiplesOf3and5(a)).toBe(b)
+			})
+		}
+	)
 })
